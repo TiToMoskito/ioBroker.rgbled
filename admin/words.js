87cfa03ca3fa42854e7bc53a3ce0e05efffd0af5
TiToMoskito <dennis@rumstein.eu>
2020-03-20 14:06:32 +0100

    Initialize

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"rgbled adapter settings": {
+		"en": "Adapter settings for rgbled",
+		"de": "Adaptereinstellungen für rgbled",
+		"ru": "Настройки адаптера для rgbled",
+		"pt": "Configurações do adaptador para rgbled",
+		"nl": "Adapterinstellingen voor rgbled",
+		"fr": "Paramètres d'adaptateur pour rgbled",
+		"it": "Impostazioni dell'adattatore per rgbled",
+		"es": "Ajustes del adaptador para rgbled",
+		"pl": "Ustawienia adaptera dla rgbled",
+		"zh-cn": "rgbled的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
