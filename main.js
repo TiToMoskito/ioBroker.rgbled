679086bfe317cab122b0860617e07632c2504257
TiToMoskito <dennis@rumstein.eu>
2020-04-12 13:50:10 +0200

    Update 0.0.2

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 "use strict";
 const adapterName 		= require('./package.json').name.split('.').pop();
 const utils 			= require("@iobroker/adapter-core");
+const tools      	    = require(utils.controllerDir + '/lib/tools.js');
 
-const WebSocketClient 	= require('websocket').client;
-
+const WebSocket 		= require('ws');
 
 let client;
-let conn;
+let connected			= false;
+let red					= 0;
+let green				= 0;
+let blue				= 0;
+
 let adapter;
 function startAdapter(options) {
     options = options || {};
@@ -16,22 +20,42 @@ function startAdapter(options) {
 
 	adapter.on('stateChange', (_id, state) => {
         if (!state || state.ack) return;
-        adapter.log.info('try to control id ' + _id + ' with ' + JSON.stringify(state));
+        //adapter.log.info('try to control id ' + _id + ' with ' + JSON.stringify(state));
         // Try to find the object
 		const id = adapter.idToDCS(_id);
 
-		if (id)
-		{
-			if (id.state === 'rainbow') {
-				if (!!state.val) {
-					conn.sendUTF("1");
-				}
+		if (id && id.channel)
+		{		 
+			if (state.val === 'false') state.val = false;
+            if (state.val === 'true')  state.val = true;
+			if (parseInt(state.val) == state.val) state.val = parseInt(state.val);
+
+			if (id.channel === 'red') {
+				red = state.val;
 			} else
-			if (id.state === 'colorWipe') {
-				if (!!state.val) {
-					conn.sendUTF("0"+ " " + adapter.getState('r').val + " " + adapter.getState('g').val +" "+ adapter.getState('b').val);
+			if (id.channel === 'green') {
+				green = state.val;
+			} else
+			if (id.channel === 'blue') {
+				blue = state.val;
+			} 		
+
+			if(connected)
+			{
+				if (id.channel === 'disconnect') {
+					disconnect();
+				} else
+				if (id.channel === 'rainbow') {
+					client.send("1");
+				} else
+				if (id.channel === 'wipeColor') {
+					client.send("0"+ " " + red + " " + green +" "+ blue);
+				}					
+			}else{
+				if (id.channel === 'connect') {
+					connect();
 				}
-			} 			
+			}
 		}
 	});
 	
@@ -40,7 +64,12 @@ function startAdapter(options) {
 	adapter.on('unload', callback => {
         try {
             if (adapter) {
-                adapter.log && adapter.log.info && adapter.log.info('terminating');
+				adapter.log && adapter.log.info && adapter.log.info('terminating');
+				disconnect();
+				client.removeEventListener("open", onConnected);
+				client.removeEventListener("connectFailed", onConnectFailed);
+				client.removeEventListener("close", onClose);
+				client.removeEventListener("error", onError);
             }
             callback();
         } catch (e) {
@@ -64,21 +93,37 @@ function startAdapter(options) {
 function createStates() 
 {
 	const states = {
-		'online': {
+		'connect': {
 			def:    false,
 			type:   'boolean',
 			read:   true,
 			write:  true,
+			role:   'button',
+			name:	'Connect',
+			desc:   'Connect to server'
+		},
+		'disconnect': {
+			def:    false,
+			type:   'boolean',
+			read:   true,
+			write:  true,
+			role:   'button',
+			name:	'Disconnect',
+			desc:   'Disconnect from server'
+		},
+		'online': {
+			def:    false,
+			type:   'boolean',
+			read:   true,
+			write:  false,
 			role:   'state',
-			min:    false,
-			max:    true,
 			name:	'Connection Status',
 			desc:   'Is the connection established'
 		},
 		'bri': {
 			def:   '100',
 			type:  'number',
-			read:  false,
+			read:  true,
 			write: true,
 			role:  'state',
 			name:	'Brightness',
@@ -87,7 +132,7 @@ function createStates()
 		'red': {
 			def:   '0',
 			type:  'number',
-			read:  false,
+			read:  true,
 			write: true,
 			role:  'state',
 			name:	'Red',
@@ -96,7 +141,7 @@ function createStates()
 		'green': { 
 			def:   '0',
 			type:  'number',
-			read:  false,
+			read:  true,
 			write: true,
 			role:  'state',
 			name:	'Green',
@@ -105,7 +150,7 @@ function createStates()
 		'blue': {
 			def:   '0',
 			type:  'number',
-			read:  false,
+			read:  true,
 			write: true,
 			role:  'state',
 			name:	'Blue',
@@ -116,9 +161,7 @@ function createStates()
 			type:   'boolean',
 			read:   true,
 			write:  true,
-			role:   'state',
-			min:    false,
-			max:    true,
+			role:   'button',
 			name:	'Rainbow effect',
 			desc:   'Rainbow effect on/off'
 		},
@@ -127,51 +170,78 @@ function createStates()
 			type:   'boolean',
 			read:   true,
 			write:  true,
-			role:   'state',
-			min:    false,
-			max:    true,
+			role:   'button',
 			name:	'Color wipe',
 			desc:   'Change color effect on/off'
 		}    
 	};
 
 	const states_list = [];
-    for (const state in states) {
+	for (const state in states) 
         states_list.push(state);
-	}
 	
-	for (let j = 0; j < states_list.length; j++) {
-        adapter.createState('root', 0, states_list[j], states[states_list[j]]);
-    }
+	for (let j = 0; j < states_list.length; j++)
+        adapter.createState('root', 0, states_list[j], states[states_list[j]]);    
 }
 
 function main() 
 {
 	adapter.subscribeStates('*');
-	createStates();
-
-	client = new WebSocketClient();
-	client.connect('ws://'+adapter.config.ipaddress+':'+adapter.config.port+'/', 'echo-protocol');
-
-	client.on('connectFailed', function(error) {
-		console.log('Connect Error: ' + error.toString());
-		adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: false, ack: true});
-	});
-	
-	client.on('connect', function(connection) {
-		conn = connection;
-		adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: true, ack: true});
-
-		connection.on('error', function(error) {
-			console.log("Connection Error: " + error.toString());
-			adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: false, ack: true});
-		});
-		connection.on('close', function() {
-			console.log('echo-protocol Connection Closed');
-			adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: false, ack: true});
-		});
-	});
+	createStates();	
+	connect();
 }
+
+function connect()
+{
+	if(!connected)
+	{
+		client = new WebSocket('ws://'+adapter.config.ipaddress+':'+adapter.config.port+'/');
+		client.addEventListener("open", onConnected);
+		client.addEventListener("connectFailed", onConnectFailed);
+		client.addEventListener("close", onClose);
+		client.addEventListener("error", onError);
+	}
+}
+
+function disconnect()
+{
+	client.close();
+	connected = false;
+	adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: false, ack: true});
+	adapter.log.info("Disconnected");
+}
+
+function onConnected()
+{
+	adapter.log.info('Connection established');
+	adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: true, ack: true});
+	connected = true;
+}
+
+function onConnectFailed(error)
+{
+	adapter.log.info('Connect Error: ' + error);
+	adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: false, ack: true});
+	connected = false;
+}
+
+function onClose(code, reason)
+{
+	adapter.log.info("Connection closed("+code+"): " + reason);
+	adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: false, ack: true});
+	connected = false;
+}
+
+function onError(error)
+{
+	adapter.log.info("Connection Error: " + error.toString());
+	adapter.setState({device: 'root', channel: 0, state: 'online'}, {val: false, ack: true});
+	connected = false;
+}
+
+
+/* if(recon && adapter.config.reconnect)
+	connect(); */
 
 // @ts-ignore parent is a valid property on module
 if (module.parent) {
